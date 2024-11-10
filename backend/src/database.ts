44da4feedcca030381254805b915392b3a8c3488
Austen Stone <austenstone@github.com>
2024-11-10 16:56:45 -0500

    Refactor database connection logic to streamline JAWSDB_URL handling and improve error logging

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -18,19 +18,18 @@ const sequelize = process.env.JAWSDB_URL ?
 
 const dbConnect = async () => {
   try {
-    const connection = process.env.JAWSDB_URL ?
-      await mysql2.createConnection(process.env.JAWSDB_URL)
-      : await mysql2.createConnection({
+    if (process.env.JAWSDB_URL) {
+      const connection = await mysql2.createConnection({
         host: process.env.MYSQL_HOST || 'localhost',
         port: parseInt(process.env.MYSQL_PORT || '3306'),
         user: process.env.MYSQL_USER || 'root',
         password: process.env.MYSQL_PASSWORD || 'octocat',
       });
-
-    const query = await connection.query(`CREATE DATABASE IF NOT EXISTS \`value\`;`,);
-
-    console.log(query);
-    await connection.end();
+  
+      await connection.query(`CREATE DATABASE IF NOT EXISTS \`value\`;`,);
+  
+      await connection.end();
+    }
   } catch (error) {
     logger.error('Unable to connect to the database', error);
     throw error;
