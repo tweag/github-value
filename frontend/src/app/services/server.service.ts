90728d1ba756e07ebf109556dea0345cacee7494
Austen Stone <austenstone@github.com>
2024-11-10 17:22:55 -0500

    Refactor server URL logic to return an empty string in production and simplify development mode handling

--- frontend/src/app/services/server.service.ts
+++ frontend/src/app/services/server.service.ts
@@ -1,12 +1,10 @@
 import { isDevMode } from '@angular/core';
 
 export const serverUrl = (() => {
-  if (process.env['BASE_URL']) {
-    return process.env['BASE_URL'];
-  } else if (isDevMode()) {
+  if (isDevMode()) {
     return 'http://localhost';
   } else {
-    return 'http://localhost';
+    return '';
   }
 })();
 
