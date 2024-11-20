9db569813da4f8ef4ee675f8b7f4a92b02c8fc67
Matt Gunter <mattg57@github.com>
2024-11-20 10:40:35 -0500

    Remove TargetValues controller, routes, and service files

--- backend/src/routes/target-values.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import TargetValuesController from '../controllers/target-values.controller.js';
-
-const router = Router();
-
-router.get('/', TargetValuesController.getTargetValues);
-router.put('/', TargetValuesController.updateTargetValues);
-
-export default router;
