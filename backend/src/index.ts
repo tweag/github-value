73e0dd57133756ed8727526e13e8a88079fd5eb0
Matt Gunter <98851101+MattG57@users.noreply.github.com>
2025-01-21 15:04:27 -0500

    Update index.ts
    console.log(`Starting app with port: ${port}`);

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import App from './app.js';
 
 const app = new App(Number(process.env.PORT) || 8080);
+console.log(`Starting node app with port: ${port}`);
 
 ['SIGTERM', 'SIGINT', 'SIGQUIT'].forEach(signal => {
   process.on(signal, async () => {
